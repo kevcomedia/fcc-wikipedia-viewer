6a4b410cce925584424d4308f6acb5face9fc325
Kev Comedia <kevcomedia@users.noreply.github.com>
2016-09-10 16:15:45 +0800

    Add gulp plugins for JavaScript

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,10 @@
 const gulp = require("gulp"),
+  jshint = require("gulp-jshint"),
+  stylish = require("jshint-stylish"),
+  babel = require("gulp-babel"),
+  rename = require("gulp-regex-rename"),
   browserSync = require("browser-sync").create();
-  
+
 gulp.task("browserSync", function() {
   browserSync.init({
     server: {
@@ -9,6 +13,27 @@ gulp.task("browserSync", function() {
   });
 });
 
+gulp.task("lint", function() {
+  return gulp.src("src/js/**/*.es6.js")
+    .pipe(jshint({
+      esversion: 6
+    }))
+    .pipe(jshint.reporter(stylish));
+});
+
+gulp.task("babel", ["lint"], function() {
+  return gulp.src("src/js/**/*.es6.js")
+    .pipe(babel({
+      presets: ["es2015"]
+    }))
+    .pipe(rename(/\.es6/, ""))
+    .pipe(gulp.dest("src/js"))
+    .pipe(browserSync.reload({
+      stream: true
+    }));
+});
+
 gulp.task("watch", ["browserSync"], function() {
+  gulp.watch("src/js/**/*.es6.js", ["babel"]);
   gulp.watch("src/*.html", browserSync.reload);
 });
