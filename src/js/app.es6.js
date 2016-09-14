4d22a33b61ee6086e1601571609a8c253ce779be
Kev Comedia <kevcomedia@users.noreply.github.com>
2016-09-14 10:55:55 +0800

    Replace query with opensearch

--- src/js/app.es6.js
+++ src/js/app.es6.js
@@ -1,24 +1,31 @@
 
-function fetchData(searchTerm) {
+(function WikipediaViewer() {
   "use strict";
 
-  return Promise.resolve($.ajax({
-    url: "https://en.wikipedia.org/w/api.php",
-    dataType: "jsonp",
-    data: {
-      action: "query",
-      format: "json",
-      prop: "pageimages|pageterms",
-      generator: "prefixsearch",
-      redirects: "1",
-      formatversion: "2",
-      piprop: "thumbnail",
-      pithumbsize: "50",
-      pilimit: "10",
-      wbptterms: "description",
-      gpssearch: searchTerm,
-      gpslimit: "10"
-    }
-  }))
-  .then(data => Promise.resolve(data.query.pages));
-}
+  function fetchData(searchTerm) {
+    // opensearch an array with the following form:
+    // [searchTerm, [titles...], [descriptions...], [links...]]
+    // The inner arrays are parallel to each other.
+    // I wish it returned an array of objects instead.
+    return Promise.resolve($.ajax({
+      url: "https://en.wikipedia.org/w/api.php",
+      dataType: "json",
+      data: {
+        action: "opensearch",
+        format: "json",
+        origin: "*",
+        search: searchTerm,
+        formatversion: "2"
+      }
+    }))
+    .then(function ([ , titles, descriptions, urls]) {
+      // Remember! If the RHS of the arrow symbol is intended to be an object,
+      // wrap in parens. Otherwise JS will think of it as a "function block".
+      return titles.map((title, index) => ({
+        title,
+        description: descriptions[index],
+        url: urls[index]
+      }));
+    });
+  }
+})();
